78240052422920365f6b3afa18e1058e7a454275
novagc <novagc@yandex.ru>
2020-05-15 08:11:10 +0900

    update python scripts delete useless source files

--- DirGenerator.py
+++ DirGenerator.py
@@ -1,3 +1,11 @@
 import os
-[os.mkdir(x) for x in ['./' + y for y in [x[:-1] if x[-1] == '\n' else x for x in open('dirs.txt', 'r',encoding='utf-8').readlines()]] if not os.path.exists(x)]
-[open(z, 'w', encoding='utf-8').close() for z in ['./' + y + './Source.cpp' for y in [x[:-1] if x[-1] == '\n' else x for x in open('dirs.txt', 'r',encoding='utf-8').readlines()]] if not os.path.exists(z)]
+import fnmatch as fm
+
+temp = [x[:-1] if x[-1] == '\n' else x for x in open('dirs.txt', 'r', encoding='utf-8').readlines()]
+
+for dir in temp:
+	if not os.path.exists(f'./{dir}'):
+		os.mkdir(f'./{dir}')
+	
+	if len([x for x in os.listdir(f'./{dir}') if fm.fnmatch(x, 'Source.*')]) == 0:
+		open(f'./{dir}/Source.cpp', 'w', encoding='utf-8').close()
--- ScreenshotRenamer.py
+++ ScreenshotRenamer.py
@@ -1,2 +1,12 @@
-import os, fnmatch
-[[os.rename(a + '/' + file, a + '/res.png') for file in os.listdir(a) if fnmatch.fnmatch(file, 'snap_screen*')] for a in [z for z in ['./' + y for y in [x[:-1] if x[-1] == '\n' else x for x in open('dirs.txt', 'r',encoding='utf-8').readlines()]] if os.path.exists(z)]]
+import os
+import fnmatch as fm 
+
+temp = [x[:-1] if x[-1] == '\n' else x for x in open('dirs.txt', 'r', encoding='utf-8').readlines()]
+
+for dir in [x for x in temp if os.path.exists(f'./{x}')]:
+	files = [x for x in os.listdir(f'./{dir}') if fm.fnmatch(x, 'snap_screen*')]
+	if len(files) == 1:
+		os.rename(f'./{dir}/{files[0]}', f'./{dir}/res.png')
+	else:
+		for file, index in enumerate(files):
+			os.rename(f'./{dir}/{file}', f'./{dir}/{index + 1}.png')
